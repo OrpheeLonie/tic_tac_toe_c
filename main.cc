825f186cda60921720959bc8cde35a7882d19809
Orphee <orphee.lonie@gmail.com>
2023-05-15 23:49:08 +0200

    [main] finish main

--- main.cc
+++ main.cc
@@ -5,8 +5,34 @@
 int main()
 {
     tic_tac_toe::TicTacToe ticTacToe;
+    tic_tac_toe::State player = tic_tac_toe::State::O;
+    std::string input;
+    int i = 0;
 
-    std::cout << ticTacToe;
+    while (i < 9 && !ticTacToe.IsWin())
+    {
+        std::cout << ticTacToe << "Player " << player << " : ";
+        std::cin >> input;
+        if (!ticTacToe.PlayMove(player, input))
+        {
+            std::cout << "Invalid move\n";
+            continue;
+        }
+
+        i++;
+
+        player = player == tic_tac_toe::State::O ? tic_tac_toe::State::X : tic_tac_toe::State::O;
+    }
+
+    if (ticTacToe.IsWin())
+    {
+        player = player == tic_tac_toe::State::O ? tic_tac_toe::State::X : tic_tac_toe::State::O;
+        std::cout << "Player " << player << " won!\n";
+    }
+    else
+    {
+        std::cout << "Draw!\n";
+    }
 
     return 0;
 }
